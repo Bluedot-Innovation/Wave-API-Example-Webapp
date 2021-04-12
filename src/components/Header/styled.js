25ac2e81921a492dab5631458538dbbf1d5890ab
Daniel Toro <toro.duque@gmail.com>
2021-04-12 14:30:26 +1000

    refactor(Header/styled.js): removed unused code

--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -1,11 +1,6 @@
-import { Logo } from '@bluedot-innovation/bluedot-ui'
 import styled from 'styled-components'
 
 export const HeaderWrapper = styled.div`
     padding: min(2%, 1rem);
     border-bottom: solid ${({ theme }) => theme.colors.PRIMARY} 2px;
-
-    ${Logo} {
-        width: 140px;
-    }
 `
